76abb025f56bd177b869ffa4d51026185dcef0ed
MadeFix <oladosulekan@yahoo.com>
2019-01-27 02:32:46 +0100

    - Firebase Integrated thru manual frameworks

---
+++ beam mobile/LekanObjc.m
@@ -0,0 +1,9 @@
+//
+//  LekanObjc.m
+//  beam mobile
+//
+//  Created by MAC on 1/26/19.
+//  Copyright © 2019 novugrid. All rights reserved.
+//
+
+ #import <Foundation/Foundation.h>
---
+++ beam mobile/UI/Cells/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  beam mobile
+//
+//  Created by MAC on 1/26/19.
+//  Copyright © 2019 novugrid. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
